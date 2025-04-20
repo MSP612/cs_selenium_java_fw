ee54727948e13781ca9b683a5c4fb74eda000f4f
MSP612 <83526913+MSP612@users.noreply.github.com>
2025-04-20 18:59:57 +0530

    Update jenkinsFile.groovy

--- jenkinsFile.groovy
+++ jenkinsFile.groovy
@@ -2,8 +2,8 @@ pipeline {
     agent any
 
     tools {
-        maven 'Maven 3.9.9'   // use the name you gave in Jenkins Maven config
-        jdk 'Java 17'         // or whatever version you're using
+        maven 'MAVEN_HOME'   // use the name you gave in Jenkins Maven config
+        jdk 'JAVA_HOME'         // or whatever version you're using
     }
 
     environment {
